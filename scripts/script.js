38d43863f80123670903b1f5b66dd84bd0b8d45d
Kevin <110263079+Dhtkevo@users.noreply.github.com>
2022-12-18 11:00:53 -0500

    Complete rps UI
    Successfully implemented all desired features

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
-let user = 0;
-let computer = 0;
+let userScore = 0;
+let computerScore = 0;
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+const outputBar = document.getElementById('output');
+const p = document.createElement('p');
+p.innerText = "";
+outputBar.appendChild(p);
+
+const scoreBoard = document.createElement('h1');
+outputBar.appendChild(scoreBoard);
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -12,42 +24,62 @@ function getUserChoice() {
     return choice;
 }
 
-const outputBar = document.getElementById('output');
-let output = document.createElement('p');
-
 function playRound(playerSelection, computerSelection) {
+    
+    computerSelection = getComputerChoice();
+    
     if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
-        user++;
-        output.textContent = `User wins! ${playerSelection} beats ${computerSelection}`;
-        outputBar.appendChild(output);
+        userScore++;
+        p.innerText =  `User wins! ${playerSelection} beats ${computerSelection}`;
+      console.log(userScore + " " + computerScore);
         return 'You Win!';
     } else if (playerSelection === computerSelection) {
-        output.textContent ='It\'s a tie!';
+        p.innerText ='It\'s a tie!';
         return 'Tie!';
     } else {
-        computer++;
-        output.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+        console.log(userScore + " " + computerScore);
+        p.innerHTML = `You Lose! ${computerSelection} beats ${playerSelection}`;
         return 'You Lose!';
     }
+
+    
 }
 
 function resetScores(){
-    user = 0;
-    computer = 0;
+    userScore = 0;
+    computerScore = 0;
 }
 
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
+const checkWinner = (playerSelection, computerSelection) => {
+    if (userScore === 5) {
+        const h2 = document.createElement('h2');
+        h2.innerText = `You Win The Game! ${userScore} to ${computerScore}`;
+        outputBar.appendChild(h2);
+        resetScores();
+    } else if (computerScore === 5) {
+        const h2 = document.createElement('h2');
+        h2.innerText = `You Lose The Game! ${computerScore} to ${userScore}`;
+        outputBar.appendChild(h2);
+        resetScores();
+    }
+}
+
 
 rock.addEventListener('click', function() {
     playRound(rock.innerText.toLowerCase(), getComputerChoice());
+    checkWinner();
+    scoreBoard.innerText = `User: ${userScore} Computer: ${computerScore}`;
 });
 
 paper.addEventListener('click', function () {
     playRound(paper.innerText.toLowerCase(), getComputerChoice());
+    checkWinner();
+    scoreBoard.innerText = `User: ${userScore} Computer: ${computerScore}`;
 });
 
 scissors.addEventListener('click', function () {
     playRound(scissors.innerText.toLowerCase(), getComputerChoice());
+    checkWinner();
+    scoreBoard.innerText = `User: ${userScore} Computer: ${computerScore}`;
 });
